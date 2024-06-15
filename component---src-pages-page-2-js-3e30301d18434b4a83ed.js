f0540d99c3a8ddb28faa68cd784c845b442028ab
halfMonkey <mhyadai@gmail.com>
2024-06-15 16:27:30 +0000

    draft

--- component---src-pages-page-2-js-3e30301d18434b4a83ed.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var s=a(7294);a(4160);t.default=()=>s.createElement("div",null,"    Hi second page")}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-3e30301d18434b4a83ed.js.map
